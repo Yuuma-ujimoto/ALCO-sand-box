3e139c09f8849c133fb5fd210579206a46220f8a
Yuuma-ujimoto <yxy.yuuma@gmail.com>
2021-11-27 12:19:09 +0900

    feat:一応他の処理も実装してみる

--- Scripts/Array.js
+++ Scripts/Array.js
@@ -3,3 +3,21 @@ const data = "aaa"
 // constだけで済む上に処理も短くていい感じ
 const dataArray = Array.isArray(data) ? data : [data]
 console.log(dataArray)
+
+// 一応他のやり方も載せる
+
+//***************************************************
+//わかりやすいけど効率悪いしletなので安全性も薄い
+let dataArray2
+if (!Array.isArray(data)){
+    dataArray2 = [data]
+}
+else {
+    dataArray2 = data
+}
+//***************************************************
+//if文一本で済むけど処理わかりにくいのと安全性がちょっとアレなコード
+let dataArray3 = data
+if (!Array.isArray(data)){
+    dataArray3 = [data]
+}
